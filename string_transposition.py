9a3886eacc3ad7b7aac7d45dc2931d7f43ba5a9f
Michelle Berry <michberr8@gmail.com>
2017-03-25 20:32:46 -0700

    Added string transposition solution

---
+++ string_transposition.py
@@ -0,0 +1,53 @@
+def is_string_transposed(s1, s2):
+    """Checks if two strings are the same
+    except that two adjacent letters are swapped.
+
+    >>> is_string_transposed("Maria", "Marai")
+    True
+
+    >>> is_string_transposed("Marin", "Marai")
+    False
+
+    >>> is_string_transposed("Mraia", "Maria")
+    True
+
+    >>> is_string_transposed("Mraai", "Maria")
+    False
+
+    """
+
+    # Fail fast if lengths don't match
+    if len(s1) != len(s2):
+        return False
+
+    i = 0
+    n_swaps = 0
+
+    while i < len(s1):
+        if s1[i] != s2[i]:
+
+            try:
+                # Check for swap
+                if s1[i] == s2[i+1] and s1[i+1] == s2[i]:
+                    n_swaps += 1
+                    i += 2
+                else:
+                    return False
+
+            except IndexError:
+                # Will throw an index error if at end of string
+                return False
+
+            # Only allow one swap in string
+            if n_swaps > 1:
+                return False
+        else:
+            i += 1
+
+    return True
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED!\n"
