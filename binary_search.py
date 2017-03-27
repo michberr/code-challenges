e90c8eae432217f51df19bd2b5b8112d7a0932f1
Michelle Berry <michberr8@gmail.com>
2017-03-26 22:39:43 -0700

    Added recursive solution to binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -37,6 +37,39 @@ def binary_search(val):
     return num_guesses
 
 
+def binary_search_recursive(val):
+    """Using binary search, find val in range 1-100. Return # of guesses.
+
+    >>> binary_search_recursive(50)
+    1
+
+    >>> binary_search_recursive(25)
+    2
+
+    >>> binary_search_recursive(75)
+    2
+
+    >>> binary_search_recursive(31)
+    4
+
+    """
+
+    assert 0 < val < 101, "Val must be between 1-100"
+
+    def _bsr(guess, val, lower, upper, num_guesses):
+        if guess == val:
+            return num_guesses
+
+        if val > guess:
+            lower = guess
+        else:
+            upper = guess
+
+        return _bsr((upper - lower)/2 + lower, val, lower, upper, num_guesses + 1)
+
+    return _bsr(50, val, 1, 100, 1)
+
+
 ##################################################
 
 if __name__ == "__main__":
